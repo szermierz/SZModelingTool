332bc86ec1c500c03abc7e31d37b66a2404a0caa
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2020-06-06 11:21:32 +0200

    Everything group

---
+++ Editor/ToolsetGroups/ToolsetEverythingGroup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace SZ.ModelingTool
+{
+    public sealed class ToolsetEverythingGroup : ToolsetGroup
+    {
+        [SerializeField]
+        private EventType[] m_ignoredEvents = default;
+        
+        protected override void OnEvent(EditorEventWrapper wrapper, SceneView sceneView, IEnumerable<Vertex> vertices)
+        {
+            if (m_ignoredEvents.Contains(wrapper.EventType))
+                return;
+
+            foreach (var tool in Tools)
+                tool.ActivateTool(wrapper, vertices, sceneView, wrapper.MousePosition);
+        }
+    }
+}
