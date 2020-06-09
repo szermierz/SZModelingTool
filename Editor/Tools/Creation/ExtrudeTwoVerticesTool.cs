b1536e871e221fe8dcfa48b2a7237be6f3e57a6b
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2020-06-09 20:44:10 +0200

    tools update

---
+++ Editor/Tools/Creation/ExtrudeTwoVerticesTool.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace SZ.ModelingTool
+{
+    public class ExtrudeTwoVerticesTool : ToolBase
+    {
+        [SerializeField]
+        private string m_vertexNameBase = "V";
+
+        public override void ActivateTool(EditorEventWrapper wrapper, IEnumerable<Vertex> vertices, SceneView sceneView, Vector2 mousePos)
+        {
+            base.ActivateTool(wrapper, vertices, sceneView, mousePos);
+
+            if(wrapper.Consume() && vertices.Any())
+            {
+                HashSet<string> names = null;
+                List<GameObject> newVertices = new List<GameObject>();
+                var parent = vertices.First().transform.parent;
+
+                foreach (var v in vertices)
+                {
+                    newVertices.Add(Instantiate(v.gameObject, parent));
+                    newVertices.Last().name = GetFreeName(parent, m_vertexNameBase, names);
+                }
+
+                Selection.objects = newVertices.ToArray();
+            }
+        }
+
+        private static string GetFreeName(Transform parent, string nameBase, HashSet<string> namesCache = null)
+        {
+            if (null == namesCache)
+            {
+                namesCache = new HashSet<string>();
+                foreach (Transform item in parent)
+                    namesCache.Add(item.name);
+            }
+
+            for(int i = 0; ; ++i)
+            {
+                var candidate = $"{nameBase} ({i})";
+                if (!namesCache.Contains(candidate))
+                {
+                    namesCache.Add(candidate);
+                    return candidate;
+                }
+            }
+        }
+    }
+}
