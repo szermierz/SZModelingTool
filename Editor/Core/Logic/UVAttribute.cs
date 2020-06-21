58d2a7918102dc8be3a5ca644af08b96c9762172
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2020-06-21 18:04:56 +0200

    WiP vertex attributes

---
+++ Editor/Core/Logic/UVAttribute.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace SZ.ModelingTool
+{
+    public class UVAttribute : VertexAttribute<UVAttribute>
+    {
+		[SerializeField]
+		private Vector2 m_uv = default;
+		public Vector2 UV
+		{
+			get => m_uv;
+			set => m_uv = value;
+		}
+    }
+}
---
+++ Editor/Core/Logic/VertexAttribute.cs
@@ -0,0 +1,12 @@
+﻿
+namespace SZ.ModelingTool
+{
+    public abstract class VertexAttribute<VertexAttributeType> : ModelingToolBehaviour
+        where VertexAttributeType : VertexAttribute<VertexAttributeType>
+    {
+        public static VertexAttributeType Create(Vertex vertex)
+        {
+            return vertex.gameObject.AddComponent<VertexAttributeType>();
+        }
+    }
+}
